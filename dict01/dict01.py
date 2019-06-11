6ebc238b93d0ed7cc4a8aa5a4c5d22d5ffda136f
redrock71 <student@napya-211-bchd.localdomain>
2019-06-11 15:09:43 -0400

    yaml work

--- dict01/dict01.py
+++ dict01/dict01.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
+## create a dictionary
+switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
 
-def main():
-    ## create a dictionary
-    switch = {'hostname' : 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor' : 'cisco'}
+## display parts of the dictionary
+print( switch['hostname'] )
+print( switch['ip'] )
 
-    ## display parts of the dictionary
-    print( switch['hostname'] )
-    print( switch['ip'] )
+## request a 'fake' key
+# print( switch['lynx'] )
 
-    ## request a "fake" key
-    print( switch.get['lynx'] )
+## request a 'fake' key with .get() method
+print( "First test - .get()" )
+print( switch.get('lynx') )
 
-    print("Second test - .get()" )
-    print( switch.get('lynx', "The key is in another castle!" ))
+print( "Second test - .get()" )
+print( switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!") )
 
-    print("Third test - .get()" )
-    print( switch.get('version'))
-
-main()
+print( "Third test - .get()" )
+print( switch.get('version') )
